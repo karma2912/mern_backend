2e92f3a20c1be89603d1b2ff5e9c153201ca6466
karma2912 <rajakyash23@gmail.com>
2024-08-04 18:31:23 +0530

    commit

--- index.js
+++ index.js
@@ -82,6 +82,6 @@ app.get("/",(req,res)=>{
     res.send("Hello from backend Karma , This Project is single handedly Done by Karma")
 })
 
-app.listen(3000,()=>{
-    console.log("Backend is running on port 3000")
+app.listen(5000,()=>{
+    console.log("Backend is running on port 5000")
 })
