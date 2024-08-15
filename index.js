e6e00c8c544b31a4a3053e0e992a075686b951df
karma2912 <rajakyash23@gmail.com>
2024-08-15 15:17:25 +0530

    just commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/api",noteRoutes)
 
 
 app.get("/",(req,res)=>{
-    res.send("Hello from backend Karma , This Project is single handedly Done by Karma boiii")
+    res.send("Hello from backend Karma , This Project is single handedly Done by Karma boiiii")
 })
 
 const port = process.env.PORT || 5000;
