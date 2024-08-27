0cbc0b82476a99e76b624ddac7fbab4bed1fb6fa
karma2912 <rajakyash23@gmail.com>
2024-08-27 13:41:14 +0530

    commmit

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const connectToMongo = require("./db.js")
 var cors = require('cors')
 const app = express()
 app.use(express.json())
-app.use(cors({
-    origin: 'https://66cc696d40f1f30008efa390--gentle-panda-ef79d1.netlify.app'
-  }));
+app.use(cors("https://66cd898e8889ce0008a3250b--gentle-panda-ef79d1.netlify.app"));
 connectToMongo();
 
 const authRoutes = require('./routes/auth.js')
